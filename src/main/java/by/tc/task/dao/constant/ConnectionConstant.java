25979201d3a997c1b3d6641c07e8331a36c36d36
OSmol <Olga_Smolyakova@outlook.com>
2017-12-11 14:23:41 +0300

    Update ConnectionConstant.java

--- src/main/java/by/tc/task/dao/constant/ConnectionConstant.java
+++ src/main/java/by/tc/task/dao/constant/ConnectionConstant.java
@@ -3,7 +3,7 @@ package by.tc.task.dao.constant;
 /**
  * Created by Y50-70 on 12.11.2017.
  */
-public class ConnectionConstant {
+public class ConnectionConstant {// эти данные нужно читать из properties-файла
     public final static String URL = "jdbc:mysql://localhost:3306/appdata";
     public final static String LOGIN = "root";
     public final static String PASSWORD = "root";
